968178634e5a140af6bdaf34346e47987ba072de
ave2me <ave2@me.com>
2019-11-30 17:02:10 +0300

    add missing db models

--- api/models.py
+++ api/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Text
+import datetime
+
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, Numeric, String, Text
 from sqlalchemy.orm import relationship
 
 from api.database import Base
@@ -12,6 +14,17 @@ class User(Base):
     hashed_password = Column(String)
 
     payment_methods = relationship("PaymentMethod", back_populates="user")
+    tokens = relationship("UserToken", back_populates="user")
+
+
+class UserToken(Base):
+    __tablename__ = "user_tokens"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    token = Column(String)
+
+    user = relationship("User", back_populates="tokens")
 
 
 class PaymentMethod(Base):
@@ -32,3 +45,28 @@ class Foundation(Base):
     name = Column(String)
     description = Column(Text)
     payment_address = Column(String)
+
+    payment_rules = relationship("PaymentRule", back_populates="foundation")
+
+
+class PaymentRule(Base):
+    __tablename__ = "payment_rules"
+
+    id = Column(Integer, primary_key=True, index=True)
+    payment_method_id = Column(Integer, ForeignKey("payment_methods.id"))
+    foundation_id = Column(Integer, ForeignKey("foundations.id"))
+    amount = Column(Numeric(precision=18))
+
+    payment_method = relationship("PaymentMethod", back_populates="payment_rules")
+    foundation = relationship("Foundation", back_populates="payment_rules")
+    payment_history = relationship("Payment", back_populates="payment_rule")
+
+
+class PaymentHistory(Base):
+    __tablename__ = "payment_history"
+
+    id = Column(Integer, primary_key=True, index=True)
+    payment_rule_id = Column(Integer, ForeignKey("payment_rules.id"))
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+    payment_rule = relationship("PaymentRule", back_populates="payment_history")
