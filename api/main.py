d62c82eb3fd32f205739aae23df6be64faea54ec
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2019-11-30 15:46:28 +0300

    Add pydantic models

---
+++ api/main.py
@@ -0,0 +1,55 @@
+from decimal import Decimal
+from enum import Enum
+
+from fastapi import FastAPI
+import pydantic
+
+app = FastAPI()
+
+
+class User(pydantic.BaseModel):
+    name: str
+    password: str
+
+
+class DbUser(User):
+    id: int
+
+
+class PaymentMethodType(str, Enum):
+    ETH = "ETH"
+
+
+class PaymentMethod(pydantic.BaseModel):
+    user_id: int
+    type: PaymentMethodType
+    private_key: str
+
+
+class DbPaymentMethod(PaymentMethod):
+    id: int
+
+
+class Foundation(pydantic.BaseModel):
+    name: str
+    description: str
+    public_key: str
+
+
+class DbFoundation(Foundation):
+    id: int
+
+
+class Payment(pydantic.BaseModel):
+    payment_method_id: int
+    foundation_id: int
+    amount: Decimal
+
+
+class DbPayment(Payment):
+    id: int
+
+
+@app.get("/user/login")
+def read_item(user: User):
+    return DbUser(id=1, name="boger", password="ugh")
