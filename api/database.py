efe882adca4c3c1fecac4e6f3ffff6038c1c07ff
Boger <kotvberloge@gmail.com>
2019-12-01 03:03:01 +0300

    :ambulance: Fix Ivan's re-creation of connection error

--- api/database.py
+++ api/database.py
@@ -6,7 +6,12 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./test.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+else:
+    connect_args = {}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
